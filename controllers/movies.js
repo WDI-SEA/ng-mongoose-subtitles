32f0ff9a02edd2771cf6f835d46b8056dfcae9a7
skylocke <mathematical.fiction@gmail.com>
2017-01-23 15:54:36 -0800

    attempting POST route

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended: true }));
+
 var models = require('../models/subtitle');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/subtitles');
@@ -39,13 +42,13 @@ router.route('/script/:id')
 // POST /movie/create/script/:templateId
 router.route('/create/script/:templateId')
 .post(function(req, res) {
-  
+  console.log("POST test");
+  // models.Subtitle.findById(req.params.templateId, function(err, subtitle) {
+  //   res.send(subtitle);
+  // });
 });
 
 // PUT /movie/edit/script/:index
-router.route('/edit/script/:index')
-.put(function(req, res) {
 
-});
 
 module.exports = router;
